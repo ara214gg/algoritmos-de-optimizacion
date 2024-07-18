5c1caa39586f32ba90b0707e1b2604b91b6eb4cd
David Pérez Antonio <david.perez.antonio51@gmail.com>
2024-07-18 01:17:51 -0600

    Se muestra de manera dinamica el proceso de optmización

--- src/Components/Background.tsx
+++ src/Components/Background.tsx
@@ -5,7 +5,7 @@ import Loader from "./Loader.tsx";
 export default function Background() {
     return (
         <div className="flex items-center justify-center min-h-screen bg-blue-100">
-            <div className="w-full max-w-xl p-4">
+            <div className="w-full max-w-screen-xl p-4">
                 <Suspense fallback={<Loader/>}>
                     <Outlet/>
                 </Suspense>
--- src/Views/NewthonRapson.tsx
+++ src/Views/NewthonRapson.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef, useState} from "react";
-import {all, create} from 'mathjs';
+import {all, create, EvalFunction} from 'mathjs';
 import "//unpkg.com/mathlive";
 import {convertLatexToAsciiMath, MathfieldElement} from "mathlive";
 
@@ -11,51 +11,105 @@ declare global {
     }
 }
 
-export default function NewthonRapson() {
-    const [originalFunction, setOriginalFunction] = useState<string>("");
-    const [derivada, setDerivada] = useState("");
+interface Values {
+    pn: number;
+    fn: number;
+    derivada: number;
+    error: number | undefined;
+}
 
+export default function NewthonRapson() {
     const config = {};
     const math = create(all, config);
+
+    const [originalLaTex, setOriginalLaTex] = useState<string>("");
+    const [derivadaLaTex, setDerivadaLaTex] = useState<string>("");
+    const [original, setOriginal] = useState<EvalFunction>(new math.ConstantNode(1));
+    const [derivada, setDerivada] = useState<EvalFunction>(new math.ConstantNode(1));
+    const [pnValue, setPnValue] = useState<string>("");
+    const [values, setValues] = useState<Values[]>([]);
+
     const mf1 = useRef<MathfieldElement | null>(null);
+
+    const ejecutarNewtoRapson = async () => {
+        setValues([]);
+        const ascciiFunction = convertLatexToAsciiMath(originalLaTex.replace("f\\left(x\\right)=", ""));
+        try {
+            let originalNode = math.parse(ascciiFunction);
+            let derivadaNode = math.derivative(originalNode, 'x');
+            setDerivadaLaTex("f^{\\prime}\\left(x\\right)=" + derivadaNode.toTex());
+            setOriginal(originalNode.compile());
+            setDerivada(derivadaNode.compile());
+            let pn = Number.parseInt(pnValue);
+            let firstElement: Values = {
+                error: undefined,
+                pn,
+                fn: originalNode.compile().evaluate({x: pn}),
+                derivada: derivadaNode.compile().evaluate({x: pn})
+            }
+            setValues(prevState => [...prevState, firstElement]);
+        } catch (error) {
+            setDerivadaLaTex("Parser Error");
+            console.log(error);
+        }
+    }
+
+    const calculateNextElement = () => {
+        const maxIndex = values.length;
+        const prevValues = values[maxIndex - 1];
+        const pn = prevValues.pn - (prevValues.fn / prevValues.derivada);
+        let newValues: Values = {
+            pn,
+            fn: original.evaluate({x: pn}),
+            derivada: derivada.evaluate({x: pn}),
+            error: Math.abs(pn - prevValues.pn)
+        }
+        if (prevValues.error !== Math.abs(pn - prevValues.pn)) {
+            setValues(prevState => [...prevState, newValues]);
+        }
+    }
 
     useEffect(() => {
         mf1.current!.mathVirtualKeyboardPolicy = "manual";
         mf1.current!.contentEditable = "false";
-        setOriginalFunction("f\\left(x\\right)=");
+        setOriginalLaTex("f\\left(x\\right)=");
     }, []);
 
-    const parse = () => {
-        console.log(convertLatexToAsciiMath(originalFunction));
-        const ascciiFunction = convertLatexToAsciiMath(originalFunction);
-        let nodeFunction = math.parse(ascciiFunction);
-        let nodeDerivado = math.derivative(nodeFunction, 'x');
-        console.log("String: " + nodeDerivado.toString());
-        console.log("Latex: " + nodeDerivado.toTex());
-        setDerivada("f^{\\prime}\\left(x\\right)=" + nodeDerivado.toTex());
-    }
+    useEffect(() => {
+        if (values.length > 0 && (values[0].error === undefined || values[values.length].error !== 0)) {
+            setTimeout(calculateNextElement, 1000);
+        }
+    }, [values]);
 
     return (
         <div>
             <h1 className="text-3xl font-bold underline text-blue-800">
                 Metodo de Newton Rapson
             </h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 my-10 gap-10">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 my-10 gap-5">
                 <math-field
-                    onInput={(evt) => setOriginalFunction(evt.currentTarget.value)}
+                    onInput={(evt) => setOriginalLaTex(evt.currentTarget.value)}
                 >
-                    {originalFunction}
+                    {originalLaTex}
                 </math-field>
                 <math-field
                     ref={mf1}
                 >
-                    {derivada}
+                    {derivadaLaTex}
                 </math-field>
+                <input
+                    value={pnValue}
+                    type="number"
+                    onInput={(e) => setPnValue(e.currentTarget.value)}
+                    className="w-full px-3 py-2 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm border-gray-300"
+                    placeholder="Valor inicial de pn"
+                />
             </div>
             <div className="my-10 min-w-[100%]">
                 <button
-                    className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-blue-700 hover:bg-blue-900"
-                    onClick={parse}
+                    disabled={(originalLaTex === "f\\left(x\\right)=" || pnValue === "")}
+                    className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-blue-700 hover:bg-blue-900 disabled:bg-blue-700/50"
+                    onClick={ejecutarNewtoRapson}
                 >
                     Ejecutar
                 </button>
@@ -72,21 +126,17 @@ export default function NewthonRapson() {
                     </tr>
                     </thead>
                     <tbody className="bg-white">
-                    <tr>
-                        <td className="border border-gray-300 px-4 py-2">0</td>
-                        <td className="border border-gray-300 px-4 py-2">1.000000</td>
-                        <td className="border border-gray-300 px-4 py-2">-0.158529</td>
-                        <td className="border border-gray-300 px-4 py-2">1.381773</td>
-                        <td className="border border-gray-300 px-4 py-2"></td>
-                    </tr>
-                    <tr>
-                        <td className="border border-gray-300 px-4 py-2">0</td>
-                        <td className="border border-gray-300 px-4 py-2">1.000000</td>
-                        <td className="border border-gray-300 px-4 py-2">-0.158529</td>
-                        <td className="border border-gray-300 px-4 py-2">1.381773</td>
-                        <td className="border border-gray-300 px-4 py-2"></td>
-                    </tr>
-                    {/* Add other rows here */}
+                    {
+                        values.map((value, index) =>
+                            <tr>
+                                <td className="border border-gray-300 px-4 py-2">{index}</td>
+                                <td className="border border-gray-300 px-4 py-2">{value.pn}</td>
+                                <td className="border border-gray-300 px-4 py-2">{value.fn}</td>
+                                <td className="border border-gray-300 px-4 py-2">{value.derivada}</td>
+                                <td className="border border-gray-300 px-4 py-2">{value.error!==undefined ? value.error : ""}</td>
+                            </tr>
+                        )
+                    }
                     </tbody>
                 </table>
             </div>
